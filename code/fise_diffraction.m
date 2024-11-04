d1a0ccd568b256ff6b0af79dd408ceef48a7b315
Brian Wandell <wandell@stanford.edu>
2024-11-03 21:21:23 -0800

    Hooking up matlab.  Making a live script. Pinhole and diffraction

--- code/fise_diffraction.m
+++ code/fise_diffraction.m
@@ -1,6 +1,4 @@
-%% After Cruikshank
-%
-% Not quite right, but getting there.
+%% Comparing two sources of blur: Pinholes and Diffraction
 %
 %
 
@@ -18,7 +16,6 @@ if pinholeD(1) == 1e-4
     assert(abs(diffractionDeg(1) - 0.28) < 0.01)
 end
 
-
 % Radius size on a wall, L meters away
 L = 0.01;
 
@@ -32,6 +29,7 @@ end
 
 
 %% Plot the diffraction pattern size vs. pinhole
+
 ieNewGraphWin;
 unit = 'mm';
 loglog(pinholeD*ieUnitScaleFactor(unit),diffractionD*ieUnitScaleFactor(unit),'-');
@@ -42,17 +40,15 @@ ylabel(sprintf('Diffraction diameter (%s)',unit));
 grid on;
 
 
-%% Cruikshank probably told us the radius, so 
-%  He says they match when the pinhole diameter is about 0.5 mm
-%  This calculation says they match when the pinhole diameter is 1 mm.
-% All consistent.
-%
-% Now make the curves for different values of L and pinholeD
+%% How does this depend on the distance to the wall?
 
 ieNewGraphWin;
 unit = 'mm';
 
-Lmeters = logspace(log10(0.005),log10(0.5),10);
+% Distances are 5, 50 and 500 millimeters
+Lmeters = logspace(log10(0.001),log10(1),5);
+match = zeros(size(Lmeters));
+
 for ii=1:numel(Lmeters)
     % Radius size on a wall, L meters away
     L = Lmeters(ii);
@@ -60,9 +56,14 @@ for ii=1:numel(Lmeters)
     p = loglog(pinholeD*ieUnitScaleFactor(unit),diffractionD*ieUnitScaleFactor(unit),'-');
     hold on;
     p.Color = [0 0 0];
+    [v,idx] = min(abs(pinholeD - diffractionD));
+    match(ii) = pinholeD(idx)*ieUnitScaleFactor(unit);
+    fprintf('L = %f (m): Equal blur at pinhole size %f (%s)\n',Lmeters(ii),match(ii),unit);
 end
 
 xlabel(sprintf('Pinhole diameter (%s)',unit));
 ylabel(sprintf('Diffraction diameter (%s)',unit));
 loglog([0;pinholeD(:)]*ieUnitScaleFactor(unit),[0;pinholeD(:)]*ieUnitScaleFactor(unit),'k--');
 grid on;
+
+%%
