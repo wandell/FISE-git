e8203573f13b4549155c3d5c07c79876d604f9b1
Brian Wandell <1837145+wandell@users.noreply.github.com>
2025-10-24 15:14:48 -0700

    Update cone PSF code and expand encoding chapter
    Expanded the human-02-encoding chapter with additional information and images about cone mosaic and point spread functions. Enhanced fise_humanConePSF.m to include more detailed scene creation, cone mosaic computation, and plotting, as well as comments and code for adjusting the point spread function and wavelength.

--- code/human/fise_humanConePSF.m
+++ code/human/fise_humanConePSF.m
@@ -1,7 +1,30 @@
-%%
+%% Image of the point spread on the cone mosaic
 
-scene = sceneCreate('point array');
+% sceneCreate('pointArray',sz,spacing,spectralType);
+scene = sceneCreate('point array',128,32);
 scene = sceneSet(scene,'h fov',0.3);
+% sceneWindow(scene);
+
+oi = oiCreate('human wvf');
+oi = oiCompute(oi,scene);
+% oiWindow(oi);
+
+%% Default cone mosaic
+cm = cMosaic;
+allE = cm.compute(oi);
+
+% Looking for another plotting method.  Asked NC.
+cm.plot('excitations',allE);
+
+%% How about changing the point SPD?
+
+scene = sceneCreate('point array',256,64);
+scene = sceneSet(scene,'h fov',0.3);
+wave = sceneGet(scene,'wave');
+monoWave = 450;
+illEnergy = zeros(numel(wave),1); 
+illEnergy(wave==monoWave) = 1; 
+scene = sceneAdjustIlluminant(scene,illEnergy);
 % sceneWindow(scene);
 
 oi = oiCreate('human wvf');
@@ -11,10 +34,16 @@ oiWindow(oi);
 %%
 cm = cMosaic;
 allE = cm.compute(oi);
+
+% Looking for another plotting method.  Asked NC.
 cm.plot('excitations',allE);
 
-%%
+%% Maybe Nicolas had a nice way to do this?
+
+%{
 wavelength = 550;
 psfRangeArcMin = 30;
-visualizePSF(oi,wavelength,psfRangeArcMin,'withSuperimposedMosaic',cm)
 
+% The cmosaic needs to be hex type?
+visualizePSF(oi,wavelength,psfRangeArcMin,'withSuperimposedMosaic',cm)
+%}
