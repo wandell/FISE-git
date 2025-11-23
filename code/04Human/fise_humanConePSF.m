4a704e09ba43721b62bb929b40cd4f7e3d19a457
Brian Wandell <wandell@stanford.edu>
2025-11-23 09:39:54 -0800

    Built site for gh-pages

--- code/04Human/fise_humanConePSF.m
+++ code/04Human/fise_humanConePSF.m
@@ -1,14 +1,47 @@
-%% Image of the point spread on the cone mosaic
+%% Point spread function (PSF) activation on the human cone mosaic
 %
-% Not sure what I should set coneslinewidth parameter
+% This script simulates how point sources of light create activation
+% patterns on the human cone photoreceptor mosaic, demonstrating both
+% spatial and chromatic effects of the eye's optical system.
 %
+% Description:
+%   Creates visualizations of point spread function (PSF) responses on the
+%   cone mosaic under different conditions. The script has two main
+%   sections:
+%
+%   1. Field Height Variation: Computes PSF activation patterns at
+%      different retinal eccentricities (0, 3, 6, and 12 degrees) using
+%      wavefront data from Polans2015 to show how optical quality varies
+%      across the visual field.
+%
+%   2. Chromatic Aberration: Demonstrates wavelength-dependent PSF changes
+%      at a fixed eccentricity (3 degrees) by computing activation patterns
+%      for monochromatic point sources at different wavelengths (550nm and
+%      450nm), illustrating the eye's chromatic aberration.
+%
+% Outputs:
+%   - SVG files showing cone activation maps for different eccentricities
+%   - SVG files showing cone activation maps for different wavelengths
+%   - All output files saved to: 
+%     fiseRootPath/chapters/images/human/02-spatial-encoding/
+%
+% Dependencies:
+%   - ISETBio toolbox
+%   - fiseRootPath.m
+%   - oiPosition function (for Polans2015 wavefront data)
+%
+% See also:
+%   cMosaic, oiPosition, sceneCreate, oiCompute
+%
+% Copyright Imageval Consulting, LLC 2025 
 
 %%
 ieInit;
+figsave = true;
+activationMap = colormap('hot');
+close(gcf);
 
 %% PSF:  Field height section
-%
-% Try 0, 2 deg and 6 deg eccentricities.
 %
 % I have noticed that in the periphery the spot sometimes ends up on a
 % S-cone and very little response is visible.  A panel-tabset flipping
@@ -32,23 +65,29 @@ for ii=1:size(eccDegs,1)
     cm.plot('excitations', allE, 'label cones',false,...
         'domain','degrees', ...
         'plot title','Activation map',...
-        'cones line width',1);
+        'cones line width',1, ...
+        'activation color map',activationMap);
 
     % This should work some day.
     % exportgraphics(gcf, fname, 'ContentType', 'vector');
     fname = sprintf('conePSF-%d-deg.svg',eccDegs(ii,1));
-    fname = fullfile(fiseRootPath,'chapters','images','human','02-encoding',fname);
-    print(gcf,fname,'-dsvg');
+    fname = fullfile(fiseRootPath,'chapters','images','human','02-spatial-encoding',fname);
+    if figsave, print(gcf,fname,'-dsvg'); end
 end
 
 %% Chromatic aberration section
 %
 
+ieInit;
+
+figsave = true;
+activationMap = hot(256); close(gcf);
+
 % Let's check just outside the fovea
-eccDegs = [2 0];
+eccDegs = [3 0];
 hfov = 0.5;
 
-% sceneCreate('pointArray',sz,spacing,spectralType);
+%% sceneCreate('pointArray',sz,spacing,spectralType);
 scene = sceneCreate('point array',256,96);
 scene = sceneSet(scene,'h fov',hfov);
 wave = sceneGet(scene,'wave');
@@ -70,15 +109,18 @@ oi = oiCompute(oi,scene,"crop",false);
 
 cm = cMosaic('eccentricityDegs',eccDegs,'sizeDegs',[hfov hfov]*1.2);
 allE = cm.compute(oi);
+allE = ieClip(allE, 0,7000);
 
 % Looking for another plotting method.  Asked NC.
-cm.plot('excitations', allE, 'label cones',true,'plot title','Activation map','cones line width',0.5);
+cm.plot('excitations', allE, 'label cones',false,...
+    'plot title','Activation map','cones line width',0.5, ...
+    'activation color map',activationMap);
 
-fname = fullfile(fiseRootPath,'chapters','images','human','02-encoding','conePSF-550.svg');
+fname = fullfile(fiseRootPath,'chapters','images','human','02-spatial-encoding','conePSF-550.svg');
 
 % This should work some day.
 % exportgraphics(gcf, fname, 'ContentType', 'vector');
-print(gcf,fname,'-dsvg');
+if figsave, print(gcf,fname,'-dsvg'); end
 
 %% How about changing the point spectral power distribution?
 
@@ -102,13 +144,16 @@ oi = oiCompute(oi,scene,"crop",true);
 %%
 % cm = cMosaic;
 allE = cm.compute(oi);
+allE = ieClip(allE, 0,7000);
 
-cm.plot('excitations', allE, 'label cones',true,'plot title','Activation map','cones line width',0.5);
+cm.plot('excitations', allE, 'label cones',false,...
+    'plot title','Activation map','cones line width',0.5, ...
+    'activation color map',activationMap);
 
-fname = fullfile(fiseRootPath,'chapters','images','human','02-encoding','conePSF-480.svg');
+fname = fullfile(fiseRootPath,'chapters','images','human','02-spatial-encoding','conePSF-480.svg');
 
 % This should work some day.
 % exportgraphics(gcf, fname, 'ContentType', 'vector');
-print(gcf,fname,'-dsvg');
+if figsave, print(gcf,fname,'-dsvg'); end
 
 %% END
